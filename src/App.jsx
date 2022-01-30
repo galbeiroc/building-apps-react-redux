195cbb428a1f758c9d2490bdacd9aa0584b483b8
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-30 17:45:11 -0500

    use PageNotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Route, Switch } from "react-router-dom";
 
 import AboutPage from "./components/about/AboutPage";
 import Header from "./components/common/Header";
+import CoursePage from "./components/courses/CoursePage";
 import HomePage from "./components/home/HomePage";
+import PageNotFound from "./components/PageNotFound";
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursePage} />
+        <Route component={PageNotFound}/>
       </Switch>
     </div>
   );
---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PageNotFound = () => (
+  <div>
+    <h3>Ooops! Page not found.</h3>
+  </div>
+);
+
+export default PageNotFound;
