2ffa6bc00942041953835db169ebf59adcef5fca
Albeiro Crespo Gutierrez <galbeiroc@Albeiros-MacBook-Pro.local>
2022-01-30 15:35:32 -0500

    build environment

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Building Apps React Redux
+
+#### Dev Environment
+* Transpile: Babel
+* Bundle: Webpack
+* Web server: Webpack
+* Generating index.html: Webpack
+* Loading changes on save: Webpack
+* One command via npm scripts!
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    historyApiFallback: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html"
+    })
+  ],
+};
